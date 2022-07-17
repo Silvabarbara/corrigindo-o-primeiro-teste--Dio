5dc9512393e75afd5527f826ac0d5332cbdbff8c
Bárbara silva <96071590+Silvabarbara@users.noreply.github.com>
2022-07-17 20:12:13 -0300

    first commit

---
+++ src/br/com/dio/primeiroPrograma.java
@@ -0,0 +1,54 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class primeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+        Livro livro1 = new Livro("O priblema dos 3 corpos", 300);
+        System.out.println(livro1);
+
+
+    /*int a =5;
+        int b = 3;
+        System.out.println("hello word!" + (a+b));
+
+         */
+    }
+}
+
+class Livro{
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+}
